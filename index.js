981d840217f67d56fae989d534dbb0d2193fabc8
Leonardoscsal1 <lsc22@aluno.ifnmg.edu.br>
2025-07-20 21:01:02 -0300

    finalizando o jogo

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 import { exit } from 'node:process';
 import * as readline from 'node:readline/promises';
+import fs from "fs"
+
+function lerArquivo(){
+    try{
+    const arquivo = fs.readFileSync('jogo.json')
+    return (JSON.parse(arquivo.toString()))
+
+    } catch{
+        return []
+    }
+}
+
+const ranking = lerArquivo()
+
+
+    const ordenar = (itemA, itemB) => {
+        return itemB.pontuacao - itemA.pontuacao
+    }
+    
+    function imprimirRanking(){
+        console.log('----- Ranking ------')
+        ranking.sort(ordenar).forEach((item) => {
+        console.log(`Nome:${jogo.nome} // Pontuação: ${item.pontuacao}`)
+        })
+    }
+
+
+
 
 const perguntas = [
     {
@@ -174,27 +202,45 @@ const rl = readline.createInterface({
 });
 
 
-    const jogo = {
-        rodada: 0,
-        premiacao: {
-            maxima: 1000000,
-            errar: 0,
-            parar: 0,
-            acertar: 10,
-            valorGanho: 10
-        },
-        encerrado: false,
-        perguntas: randomizaPerguntas([...perguntas])
-    };
-    function reniciarJogo (){
-        jogo.rodada = 0
-        jogo.premiacao.errar = 0
-        jogo.premiacao.parar = 0
-        jogo.premiacao.acertar = 10
-        jogo.premiacao.valorGanho = 10
-        jogo.perguntas = randomizaPerguntas([...perguntas])
-    }
+const jogo = {
+    rodada: 0,
+    trocas: 3,
+    premiacao: {
+        maxima: 1000000,
+        errar: 0,
+        parar: 0,
+        acertar: 10,
+        valorGanho: 10
+    },
+    encerrado: false,
+    perguntas: randomizaPerguntas([...perguntas])
+};
 
+function salvarRanking(pontuacao){
+    ranking.push(
+        {
+            nome: jogo.nome,
+            pontuacao
+        }
+    )
+}
+    
+    function reniciarJogo (){
+    jogo.rodada = 0
+    jogo.premiacao.errar = 0
+    jogo.premiacao.parar = 0
+    jogo.premiacao.acertar = 10
+    jogo.premiacao.valorGanho = 10
+    jogo.perguntas = randomizaPerguntas([...perguntas])
+}
+
+function trocarPergunta() {
+if (jogo.trocas > 0) {
+    console.log(`Você usou um pulo! Restam apenas ${jogo.trocas - 1} trocas.`);
+    jogo.trocas--;
+    jogo.rodada++;
+}
+}
 
 jogo.nome = await rl.question(`Para começar o jogo digite seu nome: `);
 
@@ -206,61 +252,75 @@ while (!jogo.encerrado) {
     console.log("=================================");
     console.log("Seu nome:", jogo.nome)
     console.log("Rodada:", (jogo.rodada + 1))
-    console.log("Premiação Total:$", (jogo.premiacao.maxima))
-    console.log("Errar:$", (jogo.premiacao.errar))
-    console.log("Parar:$", (jogo.premiacao.parar))
-    console.log("Acertar:$", (jogo.premiacao.acertar))
+    console.log("Trocas:", jogo.trocas)
+    console.log("Premiação Total:R$", (jogo.premiacao.maxima))
+    console.log("Errar:R$", (jogo.premiacao.errar))
+    console.log("Parar:R$", (jogo.premiacao.parar))
+    console.log("Acertar:R$", (jogo.premiacao.acertar))
     console.log("=================================");
     console.log(perguntaAtual.pergunta)
     perguntaAtual.alternativas.forEach(alternativas => {
         console.log(alternativas.text)
     })
     
-
-    const resposta = await rl.question("Digite a letra da resposta ou P para parar: ");
+    const resposta = await rl.question("Digite a letra da resposta, P para parar ou T para trocar a pergunta: ");
 
         if(resposta.toUpperCase() == "P"){
-            console.log(`${jogo.nome} que pena que você decidiu parar, seu prêmio foi de $${jogo.premiacao.parar} e você parou na rodada ${jogo.rodada} faltava ${5-jogo.rodada} rodadas para você ser o vencedor! `)
+            console.log(`A alternativa correta da pergunta era ${perguntaAtual.resposta}`)
+            console.log(`${jogo.nome} que pena que você decidiu parar, seu prêmio foi de R$${jogo.premiacao.parar} e você parou na rodada ${jogo.rodada} faltava ${5-jogo.rodada} rodadas para você ser o vencedor! `)
+            imprimirRanking()
+            salvarRanking(jogo.premiacao.parar)
             jogo.encerrado = true
+            
         }
         
+        else if (resposta.toUpperCase() == "T") {
+            trocarPergunta();
+            continue;
+        }   
 
         else if(resposta.toUpperCase() == perguntaAtual.resposta){
             console.log("Resposta Correta!")
+            if(jogo.premiacao.acertar >= 1000000){
+               console.log(`Parabéns ${jogo.nome} você ganhou o jogo, seu prêmio foi de R$${jogo.premiacao.acertar} e você completou todas as rodadas para ser vencedor!`)
+               const jogarNovamente = await rl.question("Você gostaria de jogar novamente? (s/n)")
+               salvarRanking(jogo.premiacao.acertar) 
+               if(jogarNovamente.toUpperCase() === 'S'){
+                    reniciarJogo()
+                    imprimirRankingb()
+
+                }
+                else{
+                    imprimirRanking()
+                    console.log("Obrigado por jogar!!")
+                    jogo.encerrado = true
+                }
+            }
             jogo.premiacao.parar = jogo.premiacao.acertar 
             jogo.premiacao.errar = jogo.premiacao.acertar /=2
             jogo.premiacao.acertar *=20
             jogo.premiacao.valorGanho = jogo.premiacao.parar
             jogo.rodada++
-            if(jogo.rodada >= 5){
-               console.log(`Parabéns ${jogo.nome} você ganhou o jogo, seu prêmio foi de $${jogo.premiacao.valorGanho} e você completou todas as rodadas para ser vencedor!`)
-               const jogarNovamente = await rl.question("Você gostaria de jogar novamente? (s/n)")
-                if(jogarNovamente.toUpperCase() === 'S'){
-                    reniciarJogo()
-
-                }
-                else{
-                    console.log("Obrigado por jogar!!")
-                    jogo.encerrado = true
-                }
-            }
-            
         }
 
 
         else {
             console.log(`${jogo.nome} você errou!!`)
             console.log(`A resposta correta era ${perguntaAtual.resposta}`)
-            console.log(`Seu prêmio foi de $${jogo.premiacao.errar} e você perdeu na rodada ${jogo.rodada} faltava ${5-jogo.rodada} rodadas para você ser o vencedor!`)
+            console.log(`Seu prêmio foi de R$${jogo.premiacao.errar} e você perdeu na rodada ${jogo.rodada} faltava ${5-jogo.rodada} rodadas para você ser o vencedor!`)
             const jogarNovamente = await rl.question("Você gostaria de jogar novamente? (s/n)")
+            imprimirRanking()
+            salvarRanking(jogo.premiacao.errar)
             if(jogarNovamente.toUpperCase() === 'S'){
                 reniciarJogo()    
                 
                 }
                 else{
+                    
                     console.log("Obrigado por jogar!!")
                     jogo.encerrado = true
                 }
         }
     }
+fs.writeFileSync(`jogo.json`, JSON.stringify(ranking))    
 rl.close()
